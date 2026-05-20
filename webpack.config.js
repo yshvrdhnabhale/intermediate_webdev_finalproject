5b3c9bf89178168537e45c1bd99ddedd7fef0326
Yashvardhan Abhale <yshvrdhn@Yashvardhans-MacBook-Air.local>
2026-05-20 17:06:00 +0530

    Fix index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   mode: "production",
   entry: "./src/script.js",
   output: {
-    filename: "bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new CopyPlugin({ patterns: [{ from: "style.css" }] }),
+  ],
 };
